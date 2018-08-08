ac161a9e132d3440172346630c3cb9e9b00dcc3f
Manbir Rakhra <manbir@hemamaps.com.au>
2018-08-08 16:03:07 +1000

    Added Client and Resource for Api In Memory Use

---
+++ NewsFeedProject/IdentityServer/Config/config.cs
@@ -0,0 +1,37 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Config
+{
+    public class config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("NewsFeedApis", "News Feed Apis")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "NewsFeedApis" }
+                }
+            };
+        }
+
+    }
+}
--- NewsFeedProject/IdentityServer/Startup.cs
+++ NewsFeedProject/IdentityServer/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer.Config;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,7 +16,10 @@ namespace IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddIdentityServer().AddDeveloperSigningCredential();
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(config.GetApiResources())
+                .AddInMemoryClients(config.GetClients());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
