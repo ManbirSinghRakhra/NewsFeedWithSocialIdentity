1fd309b9dc6eae2ce9326ce7190263ed1fe6d847
Manbir Rakhra <manbir@hemamaps.com.au>
2018-08-09 09:52:56 +1000

    created models for News and UserInfo

---
+++ NewsFeedProject/NewsFeedApis/Models/UserInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace NewsFeedApis.Models
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+    }
+}
