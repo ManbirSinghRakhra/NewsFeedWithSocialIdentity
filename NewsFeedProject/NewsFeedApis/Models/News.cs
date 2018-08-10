be136a95d27d1c611153fbe5f0c21cdd2ebb5edb
Manbir Rakhra <manbir@hemamaps.com.au>
2018-08-10 12:59:55 +1000

    Worked on validation and stuff.

--- NewsFeedProject/NewsFeedApis/Models/News.cs
+++ NewsFeedProject/NewsFeedApis/Models/News.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace NewsFeedApis.Models
     public class News
     {
         public int Id { get; set; }
-        //public string UserInfoId { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 1000, MinimumLength = 5)]
         public string Message { get; set; }
+
         public string ImageUrl { get; set; }
         public int UserInfoId { get; set; }
         public virtual UserInfo UserInfo { get; set; }
